2483664275c37f07e7b93f33d365d2cc7a759731
Raazarya12 <125205649+Raazarya12@users.noreply.github.com>
2024-11-18 23:45:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -77,7 +77,7 @@ MULTI_CLIENT = False
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 if 'DYNO' in environ:
-    ON_HEROKU = True
+    ON_HEROKU = False
 else:
     ON_HEROKU = False
 URL = environ.get("URL", "https://vj-file-store-rstl.onrender.com/")
