bcdedba02096ecd8f60208aaf2c48817ede4aae9
Raazarya12 <125205649+Raazarya12@users.noreply.github.com>
2025-01-03 16:18:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -59,8 +59,8 @@ PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), Fals
 VERIFY_MODE = bool(environ.get('VERIFY_MODE', True)) # Set True or False
 
 # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "instantearn.in") # shortlink domain without https://
-SHORTLINK_API = environ.get("SHORTLINK_API", "0b11c55f168da322b4947095cecb02550a0949f4") # shortlink api
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "modijiurl.com") # shortlink domain without https://
+SHORTLINK_API = environ.get("SHORTLINK_API", "0d93e8f077f989e7c414959f4510fcbe9d772b59") # shortlink api
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/link_Streaam_Movies/28") # how to open link 
 
 # Website Info:
